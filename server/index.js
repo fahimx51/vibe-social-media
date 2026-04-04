8c48ce4f946ac0c412c4796441828e537a4d2fc5
fahimx51 <fahim69.dev@gmail.com>
2026-04-04 16:29:03 +0600

    add MongoDB connection setup and update server port

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('<h1>Server is Live!</h1>');
 });
 
-
 app.listen(port, () => {
+    connectDB();
     console.log(`Server is running on http://localhost:${port}`);
 });
 
