84d075b89a33a29b327e9c3d813f77bfcd5051a2
fahimx51 <fahim69.dev@gmail.com>
2026-04-07 18:48:06 +0600

    Add SignIn and SignUp components
    - Created SignIn component with basic structure.
    - Created SignUp component with layout and styling for future enhancements.
    - Updated titlebar favicon and text

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function SignUp() {
+    return (
+        <div className='w-full h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col justify-center items-center'>
+            <div className='w-[90%] lg:max-w-[60%] h-[600px] bg-white rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]'>
+
+                <div className='w-full lg:w-[50%] h-full bg-white flex flex-col items-center p-[10px] gap-[20px]'>
+
+                </div>
+                <div className='md:w-[50%] h-full hidden lg:flex justify-center items-center bg-[#000000] flex-col gap-[10px] text-white text-[16px] font-semibold rounded-l-[30px] shadow-2xl shadow-black'>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
