2ee5ee1c17aca17a21a8e8177329f100f1163c6b
fahimx51 <fahim69.dev@gmail.com>
2026-04-18 00:48:00 +0600

    Add messaging functionality with Message and Conversation models; implement sendMessage and getAllMessages controllers

---
+++ server/controllers/message.controllers.js
@@ -0,0 +1,58 @@
+import uploadOnCloudinary from "../config/cloudinary";
+import Conversation from "../models/conversation.model";
+import Message from "../models/message.model";
+
+export const sendMessage = async (req, res) => {
+    try {
+        const senderId = req.userId;
+        const receiverId = req.params.receiverId;
+        const { text } = req.body;
+
+        let image;
+        if (req.file) {
+            image = await uploadOnCloudinary(req.file.path);
+        }
+
+        const message = await Message.create({
+            sender: senderId,
+            receiver: receiverId,
+            message: text,
+            image
+        });
+
+        let conversation = await Conversation.findOne({
+            participants: {
+                $all: [
+                    senderId, receiverId
+                ]
+            }
+        });
+
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+                messages: [message._id]
+            });
+        }
+        else {
+            conversation.messages.push(message._id);
+            await conversation.save();
+        }
+
+        res.status(200).json(message);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log("Error in send message controller", error.message);
+    }
+}
+
+export const getAllMessages = async (req, res) => {
+    try {
+        
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+        console.log("Error in get all message controller", error.message);
+    }
+}
